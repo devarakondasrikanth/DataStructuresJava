07cab0845b3dd50dca15948f85a1c08bbb06cdfb
Srikanth <devarakondasrikanth@users.noreply.github.com>
2018-03-12 10:55:46 -0500

    Update BinarySearchTree.java

--- src/com/srikanth/binary/searchtree/BinarySearchTree.java
+++ src/com/srikanth/binary/searchtree/BinarySearchTree.java
@@ -215,4 +215,23 @@ public class BinarySearchTree {
 			if(root.right!=null)
 			inOrder(root.right,lst);
 	}
+	
+	public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null){
+        return null;
+    }
+    
+    if(root == p || root == q){
+        return root;
+    }
+    
+    TreeNode l = lowestCommonAncestor(root.left,p,q);
+    TreeNode r = lowestCommonAncestor(root.right,p,q);
+    
+    if(l != null && r != null){
+        return root;
+    }
+    
+    return l != null ? l:r;
+    }
 }
