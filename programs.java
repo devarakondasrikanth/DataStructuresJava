962784b6e2ab966b5f478987c153360311bba619
Srikanth <devarakondasrikanth@users.noreply.github.com>
2018-03-14 10:40:48 -0500

    Create programs.java

---
+++ programs.java
@@ -0,0 +1,17 @@
+public int maxProduct(int[] A) {
+       int min;
+   int res = A[0], max = min = A[0]; 
+   for (int i = 1; i < A.length; i++) {
+	   if (A[i] > 0) {
+		   max = Math.max(max * A[i], A[i]);
+		   min = Math.min(min * A[i], A[i]);			   
+	   }
+	   else {
+		   int lastMax = max;
+		   max = Math.max(min * A[i], A[i]);
+		   min = Math.min(lastMax * A[i], A[i]);			   			   
+	   }
+	   res = Math.max(res, max);
+   }
+   return res;
+    }
